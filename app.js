aa1de2fedce5b891cd5c862aebc9e3acaa3cb6be
maximilianallan <maximilian.allan@gmail.com>
2014-10-14 23:50:28 +0100

    updated to new coordinates

--- app.js
+++ app.js
@@ -1,39 +1,108 @@
 // create module for custom directives
-var d3DemoApp = angular.module('d3DemoApp', []);
+//var d3DemoApp = angular.module('d3DemoApp', []);
 
 // controller business logic
-d3DemoApp.controller('MiniMap', function AppCtrl ($scope) {
+//d3DemoApp.controller('MiniMap', function AppCtrl ($scope) {
+//});
 
-});
+//var currentMousePos = { x: -1, y: -1 };
+//$(document).mousemove(function(event) {
+//    currentMousePos.x = event.pageX;
+//    currentMousePos.y = event.pageY;
+//});
 
-
-function test(x){
-
-  alert(x);
+function drawPieChart(id,data){
   
+  var color = d3.scale.category20c();
+  var width = 250;
+  var height = 250;
+  var r = width/2;
+  
+  var vis = d3.select('#'+id)
+    .append("svg:svg")
+    .data([data])
+    .attr("width", width)
+    .attr("height", height)
+    .append("svg:g")
+    .attr("transform", "translate(" + r + "," + r + ")");
+    
+    var pie = d3.layout.pie()
+                .value(function(d){return d.value;});
+
+    // declare an arc generator function
+    var arc = d3.svg.arc().outerRadius(r);
+
+    // select paths, use arc generator to draw
+    var arcs = vis.selectAll("g.slice").data(pie).enter().append("svg:g").attr("class", "slice");
+    arcs.append("svg:path")
+        .attr("fill", function(d, i){
+            return color(i);
+        })
+        .attr("d", function (d) {
+            // log the result of the arc generator to show how cool it is :)
+            //console.log(arc(d));
+            return arc(d);
+        });
+
+    // add the text
+    arcs.append("svg:text").attr("transform", function(d){
+          d.innerRadius = 0;
+          d.outerRadius = r;
+        return "translate(" + arc.centroid(d) + ")";}).attr("text-anchor", "middle").text( function(d, i) {
+        return data[i].label;}
+        );
+
 }
 
 
-function buildMapNew(){
-
-  var WIDTH = 1920;
-  var HEIGHT = 1057;
-
-  //pull svg elemnts out of svg file
-  //create svg groups to represent layers
-  //find out how to bind data to groups
-  //find out hwo to bind data to elements
-  //possibly have each group in a single json file
+function showData(id){
   
-  d3.select("body")
-    .append("object")
-      .attr("data", "test_nobg.svg")
-      .attr("width", WIDTH)
-      .attr("height", HEIGHT)
-      .on("click", function(d,i) { alert("Hello world"); }) 
-      .attr("type", "image/svg+xml");
-      
+  var humanInfo = $("#human-info");
+  var techInfo = $("#tech-info");
+  
+  humanInfo.empty();
+  techInfo.empty();
+  
+  $("#human-info").append("<h2> Social/Human info on " + $("#"+id).attr("path-name") + "</h2>");
+  d3.json("well_hs.json",function(data) {
     
+    drawPieChart("human-info",data[0].data);
+    
+  });
+  
+  techInfo.append("<h2> Technical info on " + $("#"+id).attr("path-name") + "</h2>");
+  d3.json("well_tech.json",function(data) {
+    
+    drawPieChart("tech-info",data[0].data);
+  
+  });
+  
+}
+
+function highlightGroup(id){
+
+  var cl = $("#"+id).attr("class");
+  d3.selectAll("."+cl)
+    .attr("stroke","blue");
+
+}
+
+function highlightSegment(id){
+  
+  d3.select("#"+id)
+    .attr("stroke","red")
+    .append("svg:title");
+      
+}
+
+function clearAllHighlights(id){
+  
+ // d3.selectAll("path")
+  //  .attr("stroke","black")
+  
+  d3.select("#"+id)
+    .attr("stroke","black")
+  
 }
 
 function drawPath(start, points){
@@ -44,12 +113,21 @@ function drawPath(start, points){
     return path;
 }
 
-function buildMap(){
+var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function(d) {
+              return "<strong>"+d.boundary+"</strong>";
+            });
 
+
+function buildMap(){
   
-  var width = 1920;
-  var height = 1424;
-    
+  var width = 1600;
+  var height = 1500; 
+  
+
+   
   var lineFunction = d3.svg.line()
                           .x(function(d) { return d.x; })
                           .y(function(d) { return d.y; })
@@ -66,24 +144,31 @@ function buildMap(){
                                       
 
         
-  var canvas = d3.select("body")
+  var canvas = d3.select("#map")
                      .append("svg")
                      .attr("width",width)
                      .attr("height",height);
-                     
+  
+  canvas.call(tip);
+  
   d3.json("plan.json",function(data) {
             
-        
+       
 
     canvas.selectAll("path") //get an empty selector (as there are no polygons)
           .data(data) //join an array of data with the current selection
           .enter() //selection is split up into enter(), update(), exit()
             .append("path")
             .attr("d", function(d) { return drawPath(d["type"],d["data"]) + ' Z'} )
-            .on('click', function(d) { test(d["boundary"]); })
+            .attr("id", function(d) { return d["tag"]; } )
+            .attr("class", function(d) { return d["group"];} )
+            .on('click', function(d) { showData(d["tag"]); })
+            .on('mouseenter', function(d) { tip.show(d); highlightSegment(d["tag"]); } )  
+            .on('mouseleave', function(d) { tip.hide(d); clearAllHighlights(d["tag"]); })
             .attr("stroke","black")
+            .attr("path-name", function(d) { return d["boundary"]; } )
             .attr("fill","none")
-            .attr("stroke-width",1); 
+            .attr("stroke-width",5); 
         
         
   });
